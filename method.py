05f93aa029efe177155d3fccbc41a7369331a477
codezy7 <64915468+codezy7@users.noreply.github.com>
2020-09-30 19:48:19 +0530

    Delete method.py

--- method.py
+++
@@ -1,9 +0,0 @@
-def pay_extra( self, working = False, hour):
-    for i in range(0,24):
-        if (hour>=20 and hour<=8):
-            return true
-            break
-        else:
-            return false
-
-pay_extra(True,11)
