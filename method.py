9be6c504fadea9dceb87ad90d1aaf85ca339db2e
codezy7 <64915468+codezy7@users.noreply.github.com>
2020-09-30 19:46:41 +0530

    Add files via upload

---
+++ method.py
@@ -0,0 +1,9 @@
+def pay_extra( self, working = False, hour):
+    for i in range(0,24):
+        if (hour>=20 and hour<=8):
+            return true
+            break
+        else:
+            return false
+
+pay_extra(True,11)
